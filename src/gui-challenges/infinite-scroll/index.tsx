4d62fd2aab078897b5669fd3eda8a8271992f5e4
vinay <vinay@janaspandana.in>
2023-05-11 21:55:11 +0530

    develop infinite scroll gui challenge:wip

--- src/gui-challenges/infinite-scroll/index.tsx
+++ src/gui-challenges/infinite-scroll/index.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+
 function InfiniteScroll() {
-  return <div>Infinite Scroll</div>;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://api.github.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        setData(result);
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default InfiniteScroll;
